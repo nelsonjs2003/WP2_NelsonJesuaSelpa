e521114c0e574be7325abbbff772345e4d6a9a77
NELSON2003 <nelsonjesuaselpa30@gmail.com>
2024-04-28 00:44:10 +0700

    pertemuan 1 tugas 1

---
+++ app/Controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+    class contoh1 extends CI_Controller
+{
+ public function index()
+ {
+    echo "<h1>Perkenalkan</h1>";
+    echo"Nama saya Nelson Jesua Selpa
+        Saya tinggal di Pontianak
+        saya suka bermain Futsal Dan Takraw";
+ }
+}
